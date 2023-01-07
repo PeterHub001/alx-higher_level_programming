60e41bbb5c2473bbc5e6be59d967efab439ebc5c
Ishado Emmanuel <peter.ishado@gmail.com>
2023-01-07 01:14:35 -0800

    nested loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i != j and j > i:
-            if i !== 8 and j == 9:
+            if i == 8 and j == 9:
                 print('{}{}'.format(str(i), str(j)))
             else:
                 print('{}{}'.format(str(i), str(j)), end=', ')
