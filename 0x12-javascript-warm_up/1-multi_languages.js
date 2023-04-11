c4f5292fe8dcc1015895a8689a2b55b521912884
Ishado Emmanuel <peter.ishado@gmail.com>
2023-04-11 11:01:40 -0700

    correction done array removed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-myArray.forEach(element => console.log(element));
+console.log('C is fun'); console.log('Python is cool'); console.log('JavaScript is amazing');
