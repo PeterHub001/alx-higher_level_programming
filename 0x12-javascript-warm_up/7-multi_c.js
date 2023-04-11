8a98cca30542a2c53aafbb741a4a70de5f4c133d
Ishado Emmanuel <peter.ishado@gmail.com>
2023-04-11 11:21:12 -0700

    corrected

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
+#!/usr/bin/node
 const myArgs = process.argv.slice(2);
 
 if (isNaN(myArgs[0])) {
   console.log('Missing number of occurrences');
 } else {
-  let i = 0;
-  while (i < parseInt(myArgs[0]) {
-    console.log("C is fun");
-    i++;
+  for (let i = 0; i < parseInt(myArgs[0]); i++) {
+    console.log('C is fun');
   }
 }
