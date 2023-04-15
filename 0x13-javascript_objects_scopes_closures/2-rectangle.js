2ebb24516a6c759b1502110e4f7351598cc1228d
Ishado Emmanuel <peter.ishado@gmail.com>
2023-04-15 01:39:23 -0700

    output

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {}; // if w or h is not valid, return an empty object
-    }
-    this.width = w;
-    this.height = h;
-  }
-}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {};
+  };
+};
+
