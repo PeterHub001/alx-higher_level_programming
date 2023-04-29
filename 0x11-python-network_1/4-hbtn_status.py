e3ad7a2dfa89fbda93fb534bcd9406666ba7ac05
Ishado Emmanuel <peter.ishado@gmail.com>
2023-04-29 07:39:48 -0700

    using request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
 
     import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
+    response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}".format(
         response.text.__class__, response.text))
