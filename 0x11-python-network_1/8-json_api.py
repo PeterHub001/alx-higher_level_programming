5ed9bf53b408a3fe9ed889dce56dc3ed2fdc4f95
Ishado Emmanuel <peter.ishado@gmail.com>
2023-04-29 08:22:58 -0700

    take a letter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,27 +4,25 @@ Python script that takes in a letter, and sends a POST request to the
 passed URL with the letter as a parameter
 """
 if __name__ == "__main__":
-
     import requests
     import sys
 
-    url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) != 2:
-        print("No result")
+    if len(sys.argv) == 1:
+        q = ""
     else:
-        letter = sys.argv[1]
-        if letter:
-            data = {'q': sys.argv[1]}
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+
+    response = requests.post(url, data=data)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response["id"], json_response["name"]))
         else:
-            data = {'q': ''}
-        response = requests.post(url, data)
-        try:
-            json_response = response.json()
-            if not json_response.get('id'):
-                print("No result")
-            else:
-                print("[{}] {}".format(json_response.get('id'),
-                                       json_response.get('name')))
-        except:
-            print("Not a valid JSON")
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
