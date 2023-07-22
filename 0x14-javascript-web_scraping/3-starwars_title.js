1ea2523b1e5a7bdf23e8c872065fa3b9dd03490f
Ishado Peter <peter.ishado@gmail.com>
2023-07-22 03:30:36 -0700

    corrected bug

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+const myArray = process.argv.slice(2);
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(JSON.parse(body).title);
-});
+if (Number.isInteger(parseInt(myArray[0]))) {
+  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
+    if (error) console.error('error:', error); // Print the error if one occurred
+    console.log(JSON.parse(body).title);
+  });
+}
